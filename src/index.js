08e1dff38a443e8dbaf5928b11f8f4801b9c4f35
whaleism <230928187+whaleism@users.noreply.github.com>
2025-09-17 13:44:52 -0500

    updated apiKey

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "3f3b4696cb6569teb708ec8264d1ad5o";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
